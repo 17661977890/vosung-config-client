e59af48ae62896145f35c0ff4e18d4254bd65b34
彬 <17661977890@163.com>
2019-04-12 09:15:02 +0800

    配置中心对应的得应用客户端

---
+++ src/main/java/com/example/vosungconfigclient/VosungConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.vosungconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VosungConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VosungConfigClientApplication.class, args);
+	}
+
+}
